434f710e18f8016b510b3a1df3ffe22ac846839d
mk-pmb <mk-pmb@users.noreply.github.com>
2021-03-13 21:21:11 +0100

    v0.1.5: lcTag, extras/setupClassNameEventHandlers

---
+++ extras/setupClassNameEventHandlers.js
@@ -0,0 +1,34 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false, browser: true */
+/* -*- tab-width: 2 -*- */
+(function () {
+  'use strict';
+  function rt(opt, ev) {
+    var tgt = ev.target, tn = (tgt || false).tagName, cls, fun;
+    if (!tn) { return; }
+    cls = /\S+/.exec(tgt.className || '');
+    if (!cls) { return; }
+    cls = cls[0];
+    fun = ((opt.cbPrefix || '') + tn.toLowerCase()
+      + '_' + cls.replace(/\-/g, '_') + '_' + ev.type);
+    fun = (opt[fun] || opt.unrouted);
+    if (!fun) { return; }
+    ev.clsTrig = cls;
+    ev.clsFunc = fun;
+    if (ev.clientX !== undefined) {
+      ev.elemBounds = tgt.getBoundingClientRect();
+      ev.elemX = ev.clientX - ev.elemBounds.left;
+      ev.elemY = ev.clientY - ev.elemBounds.top;
+    }
+    return fun.call(tgt, ev);
+  }
+  window.setupClassNameEventHandlers = function setup(opt, on) {
+    var tgt = opt.hookOnto, cb;
+    if (!on) { on = (opt.on || {}); }
+    if (on === '=') { on = opt; }
+    cb = rt.bind(null, on);
+    cb.on = on;
+    function inst(m) { tgt['on' + m] = cb; }
+    if (tgt) { String(opt.events || '').replace(/\w+/g, inst); }
+    return cb;
+  };
+}());
